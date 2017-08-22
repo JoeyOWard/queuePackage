2ce0495c7ad2419c5aaf31b8ad8e08cfe92480ff
Joe Ward <joe.ward@enta.net>
2017-08-22 16:48:23 +0100

    Adding in provider and composer

---
+++ packages/jollie/emailsender/src/EmailSenderProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jollie\EmailSendor;
+
+use Illuminate\Support\ServiceProvider;
+
+class EmailSenderProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
